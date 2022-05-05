8022b44edf30e25baea786a5ac0c77469f27c765
TinManTex <tinman.squad@gmail.com>
2022-05-06 07:33:33 +1200

    just jiggling bits to try and get away from caps defender false positive

--- SnakeBite/Properties/Settings.Designer.cs
+++ SnakeBite/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SnakeBite.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -73,7 +73,7 @@ namespace SnakeBite.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.9.0.6")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.9.1.0")]
         public string LastSBVersion {
             get {
                 return ((string)(this["LastSBVersion"]));
